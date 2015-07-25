264289d3a58797fff6682ea95f1b5ca35771005e
Elliot Hughes <elliot.hughes@gmail.com>
2015-07-25 19:57:06 +0100

    Read uniform angles with uniform distances from Dan's data

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
-import ConfigParser
+from ConfigParser import SafeConfigParser as ConfigParser
 import argparse
 
 import scene
+import read_ios
 
 def main():
     parser = argparse.ArgumentParser("Visualise clews")
     args = parser.parse_args()
 
-    config = ConfigParser.ConfigParser()
+    config = ConfigParser()
     config.read(["properties.ini"])
 
     host = config.get("server", "host")
     port = config.getint("server", "port")
 
-    scene.visualise_path(
-        scene.position(
-            scene.spiral_stair_case(100, 3)))
+    scene.visualise_path(scene.position(read_ios.retrieve_test("test_1")))
 
 if __name__ == "__main__":
     main()
