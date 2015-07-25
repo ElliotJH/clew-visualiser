48be9b6ab14d91e818b7f67e9503c05fbba7a789
Elliot Hughes <elliot.hughes@gmail.com>
2015-07-25 15:42:08 +0100

    Framework around speaking to the api

---
+++ client.py
@@ -0,0 +1,9 @@
+import requests
+
+class Client(object):
+    def __init__(host, port):
+        self.host = host
+        self.port = port
+        
+    def get_path(location_id, path_id):
+        pass
